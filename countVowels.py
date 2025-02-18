46e3839c345eb241e61264b118f72f68ac106b4f
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-02-18 21:16:52 +0500

    Updated

--- countVowels.py
+++ countVowels.py
@@ -1,8 +1,7 @@
 vowels = ['a', 'e', 'i', 'o','u']
 word = input('Enter a word: ')
 count = 0
-for i in range(len(vowels)):
-    for letter in word:
-        if i == letter:
-            count +=1
-            
+for letter in word:
+    if letter in vowels:
+        count+=1
+print(count)
