ed48d884baef6fb1d7b36582912470dcc32469c0
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-06 21:57:32 +0500

    Updated

--- Reverse.java
+++
@@ -1,6 +0,0 @@
-import Util.Scanner;
-public class Reverse{
-    public static void main(String[]arg){
-        System.err.println("hello");
-    }
-}
---
+++ ReverseMultiplicationTable.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ReverseMultiplicationTable {
+    public static void main(String[] arg) {
+        Scanner in = new Scanner(System.in);
+        int[] table1 = new int[10];
+        int[] table2 = new int[10];
+
+        System.out.print("Enter a number, you want table of: ");
+        int number = in.nextInt();
+
+        System.out.println("Multiplication table of " + number + " and its reverse:");
+
+        for (int i = 0; i < 10; i++) {
+            table1[i] = number * (i + 1);
+            table2[9 - i] = table1[i];   
+            System.out.println(number + " x " + (i + 1) + " = " + table1[i]); 
+        }
+
+        System.out.println("\nReversed Multiplication table of " + number + ":");
+        for (int i = 0; i<10; i++){
+            System.out.println(number + " x " + (10-i) + " = " + table2[i]);
+        }
+
+        in.close();
+    }
+}
